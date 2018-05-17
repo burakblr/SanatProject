d51277e677915e903bf9a1db89422cdfdb313356
Ahmet Burak BİLİR <bilirahmetburak@gmail.com>
2018-05-18 02:17:08 +0300

    SanatProject

---
+++ SanatProject/Sanat.MvcWebUI/Entity/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Sanat.MvcWebUI.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Sanat.MvcWebUI.Entity
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("dataConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ SanatProject/Sanat.MvcWebUI/Identity/IndentityInitializer.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Sanat.MvcWebUI.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Sanat.MvcWebUI.Identity
+{
+    public class IdentityInitializer : CreateDatabaseIfNotExists<IndentityDataContext>
+    {
+        protected override void Seed(IndentityDataContext context)
+        {
+            // Rolleri
+            if (!context.Roles.Any(i => i.Name == "admin"))
+            {
+                var store = new RoleStore<ApplicationRole>(context);
+                var manager = new RoleManager<ApplicationRole>(store);
+                var role = new ApplicationRole() { Name = "admin", Description = "admin rolü" };
+                manager.Create(role);
+            }
+
+            if (!context.Roles.Any(i => i.Name == "user"))
+            {
+                var store = new RoleStore<ApplicationRole>(context);
+                var manager = new RoleManager<ApplicationRole>(store);
+                var role = new ApplicationRole() { Name = "user", Description = "user rolü" }; ;
+                manager.Create(role);
+            }
+
+            if (!context.Users.Any(i => i.Name == "mustafademir"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser() { Name = "mustafa", Surname = "demir", UserName = "mustafademir", Email = "dmrrrr.mstf@gmail.com" };
+
+                manager.Create(user, "1234567");
+                manager.AddToRole(user.Id, "admin");
+                manager.AddToRole(user.Id, "user");
+            }
+
+            if (!context.Users.Any(i => i.Name == "ahmetbilir"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser() { Name = "ahmet", Surname = "bilir", UserName = "ahmetbilir", Email = "ahmetbilir@gmail.com" };
+
+                manager.Create(user, "1234567");
+                manager.AddToRole(user.Id, "user");
+            }
+
+
+
+            base.Seed(context);
+        }
+    }
+}
